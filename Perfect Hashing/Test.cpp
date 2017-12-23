04bbca6bdf4ab232f798bc26ddeb8da92db7e306
shankhadeep-ghoshal <groza.henrymarvin@gmail.com>
2017-12-23 13:29:57 +0300

    Final verifications completed

--- Perfect Hashing/Test.cpp
+++ Perfect Hashing/Test.cpp
@@ -9,6 +9,7 @@ int main() {
 	
 	vector<unsigned int> array;
 	for (int i = 0; i < 11; i++)array.push_back(i);
+	array.push_back(5);
 	for (int i = 55; i > 45; i--)array.push_back(i + 3);
 	for (int i = 15; i < 25; i++)array.push_back(i + 2);
 	PerfectHash p(array);
